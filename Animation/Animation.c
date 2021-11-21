33e335f07ba1acd6587ef63e54dca34d9cd32b9b
Philip Mann <philipmann88@gmail.com>
2021-11-21 14:08:12 -0500

    hsl optimizations

--- Animation/Animation.c
+++ Animation/Animation.c
@@ -32,12 +32,13 @@
 #include <string.h>
 
 #include <libavcodec/avcodec.h>
-
+#include <libavformat/avformat.h>
 #include <libavutil/opt.h>
 #include <libavutil/imgutils.h>
 #include <math.h>
 #include <process.h>
 #include <windows.h>
+#include <time.h>
 
 #define M_3_PI 0.954929658551372014613
 #define NUM_THREADS 12
@@ -90,6 +91,18 @@ struct Complex complex_exp(struct Complex c) {
     return from_polar(exp(c.re), c.im);
 }
 
+struct Complex complex_sin(struct Complex c) {
+    return (struct Complex){ sin(c.re)* cosh(c.im), cos(c.re)* sinh(c.im) };
+}
+
+struct Complex complex_cos(struct Complex c) {
+    return (struct Complex){ cos(c.re)* cosh(c.im), -1 * sin(c.re)* sinh(c.im) };
+}
+
+struct Complex scalar_mul(struct Complex a, double b) {
+    return (struct Complex) { a.re* b, a.im* b };
+}
+
 struct rgb {
     uint8_t r;
     uint8_t b;
@@ -98,76 +111,16 @@ struct rgb {
 
 struct rgb color(struct Complex c) {
     double hs = (complex_arg(c) * M_3_PI);
-    double x = 1 - fabs(fmod(hs, 2.0) - 1);
-    struct rgb result;
-    switch ((int)hs) {
-    case 0:
-        result.r = 255;
-        result.g = 255 * x;
-        result.b = 0;
-        return result;
-    case 1:
-        result.r = 255 * x;
-        result.g = 255;
-        result.b = 0;
-        return result;
-    case 2:
-        result.r = 0;
-        result.g = 255;
-        result.b = 255 * x;
-        return result;
-    case 3:
-        result.r = 0;
-        result.g = 255 * x;
-        result.b = 255;
-        return result;
-    case 4:
-        result.r = 255 * x;
-        result.g = 0;
-        result.b = 255;
-        return result;
-    case 5:
-        result.r = 255;
-        result.g = 0;
-        result.b = 255 * x;
-        return result;
-    case 6:
-        result.r = 255;
-        result.g = 0;
-        result.b = 0;
-        return result;
-    }
-}
-
-
-static void encode(AVCodecContext* enc_ctx, AVFrame* frame, AVPacket* pkt,
-    FILE* outfile)
-{
-    int ret;
-
-    /* send the frame to the encoder */
-    if (frame)
-        printf("Send frame %3"PRId64"\n", frame->pts);
-
-    ret = avcodec_send_frame(enc_ctx, frame);
-    if (ret < 0) {
-        fprintf(stderr, "Error sending a frame for encoding\n");
-        exit(1);
-    }
-
-    while (ret >= 0) {
-        ret = avcodec_receive_packet(enc_ctx, pkt);
-        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
-            return;
-        else if (ret < 0) {
-            fprintf(stderr, "Error during encoding\n");
-            exit(1);
-        }
-
-        printf("Write packet %3"PRId64" (size=%5d)\n", pkt->pts, pkt->size);
-        fwrite(pkt->data, 1, pkt->size, outfile);
-        av_packet_unref(pkt);
-    }
+    double x = 255 * (1 - fabs(fmod(hs, 2.0) - 1));
+    struct rgb results[7];
+    results[0] = (struct rgb){ 255 , x, 0};
+    results[1] = (struct rgb){ x , 255, 0 };
+    results[2] = (struct rgb){ 0 , 255, x };
+    results[3] = (struct rgb){ 0 , x, 255 };
+    results[4] = (struct rgb){ x , 0, 255 };
+    results[5] = (struct rgb){ 255 , 0, x };
+    results[6] = (struct rgb){ 255 , 0, 0 };
+    return results[(int)hs];
 }
 
 struct rgb get_color(int x, int y, int i) {
@@ -206,6 +159,25 @@ void prepare_frame_thread(void* params) {
     }
 }
 
+void prepare_sin_frame_thread(void* params) {
+    struct RenderThreadParams* p = params;
+    AVFrame* frame = p->frame;
+    double scaling_factor = 1.0 / 65;
+    double start_re = -1 * (long long)(frame->width) / 2.0;
+    double start_im = -1 * (long long)(frame->height) / 2.0;
+    double i_q = p->i / 300.0;
+    for (int y = p->id; y < frame->height; y += NUM_THREADS) {
+        for (int x = 0; x < frame->width; x++) {
+            struct Complex in = { (start_re + x) * scaling_factor, (start_im + frame->height - y) * scaling_factor };
+            struct Complex out = complex_add(scalar_mul(complex_pow(in, 2), 1 - i_q), scalar_mul(complex_pow(in, 3), i_q));
+            struct rgb c = color(out);
+            int start = frame->linesize[0] * y + x * 4;
+            frame->data[0][start] = c.r;
+            frame->data[0][start + 1] = c.g;
+            frame->data[0][start + 2] = c.b;
+        }
+    }
+}
 
 
 void prepare_frame_multithreaded(int i, AVFrame* frame) {
@@ -216,7 +188,7 @@ void prepare_frame_multithreaded(int i, AVFrame* frame) {
     }
     for (int t = 0; t < NUM_THREADS; t++) {
         params[t] = (struct RenderThreadParams){ t, i, frame };
-        threads[t] = _beginthread(prepare_frame_thread, 0, params + t);
+        threads[t] = _beginthread(prepare_sin_frame_thread, 0, params + t);
     }
     WaitForMultipleObjects(NUM_THREADS, threads, TRUE, INFINITE);
     free(params);
@@ -245,134 +217,84 @@ void prepare_frame(int i, AVFrame* frame) {
     }
 }
 
-int main(int argc, char** argv)
-{
-    const char* filename, * codec_name;
-    const AVCodec* codec;
-    AVCodecContext* c = NULL;
-    int i, ret, x, y;
-    FILE* f;
-    AVFrame* frame;
-    AVPacket* pkt;
-    uint8_t endcode[] = { 0, 0, 1, 0xb7 };
+int main(int argc, char** argv) {
+    char* filename = argv[1];
+    AVFormatContext* formatCtx = avformat_alloc_context();
+    formatCtx->oformat = av_guess_format("mp4", NULL, NULL);
+    AVIOContext* ioCtx;
+    avio_open2(&ioCtx, filename, AVIO_FLAG_WRITE, NULL, NULL);
+    formatCtx->pb = ioCtx;
 
-    if (argc <= 1) {
-        fprintf(stderr, "Usage: %s <output file>\n", argv[0]);
-        exit(0);
-    }
-    filename = argv[1];
-    codec_name = "h264_nvenc";
 
-    /* find the mpeg1video encoder */
-    codec = avcodec_find_encoder_by_name(codec_name);
-    if (!codec) {
-        fprintf(stderr, "Codec '%s' not found\n", codec_name);
-        exit(1);
-    }
+    AVCodec* codec = avcodec_find_encoder_by_name("h264_nvenc");
+    AVCodecContext* cdctx = avcodec_alloc_context3(codec);
+    cdctx->width = 2560;
+    cdctx->height = 1440;
+    cdctx->time_base = (AVRational){ 1, 60 };
+    cdctx->framerate = (AVRational){ 60, 1 };
+    cdctx->bit_rate = 24000000;
+    cdctx->gop_size = 30;
+    cdctx->pix_fmt = AV_PIX_FMT_RGB0;
 
-    c = avcodec_alloc_context3(codec);
-    if (!c) {
-        fprintf(stderr, "Could not allocate video codec context\n");
-        exit(1);
-    }
+    AVStream* stream = avformat_new_stream(formatCtx, codec);
+    avcodec_parameters_from_context(stream->codecpar, cdctx);
+    stream->time_base = cdctx->time_base;
 
-    pkt = av_packet_alloc();
-    if (!pkt)
-        exit(1);
+    av_dump_format(formatCtx, 0, filename, 1);
+    avformat_write_header(formatCtx, NULL);
 
-    /* put sample parameters */
-    c->bit_rate = 24000000;
-    /* resolution must be a multiple of two */
-    c->width = 2560;
-    c->height = 1440;
-    /* frames per second */
-    c->time_base = (AVRational){ 1, 60 };
-    c->framerate = (AVRational){ 60, 1 };
+    double start = clock();
 
-    /* emit one intra frame every ten frames
-     * check frame pict_type before passing frame
-     * to encoder, if frame->pict_type is AV_PICTURE_TYPE_I
-     * then gop_size is ignored and the output of encoder
-     * will always be I frame irrespective to gop_size
-     */
-    c->gop_size = 30;
-    //c->max_b_frames = 2;
-    c->pix_fmt = AV_PIX_FMT_RGB0;
-
-    if (codec->id == AV_CODEC_ID_H264)
-        av_opt_set(c->priv_data, "preset", "slow", 0);
-
-    /* open it */
-    ret = avcodec_open2(c, codec, NULL);
-    if (ret < 0) {
-        fprintf(stderr, "Could not open codec: %s\n", av_err2str(ret));
-        exit(1);
-    }
-
-    f = fopen(filename, "wb");
-    if (!f) {
-        fprintf(stderr, "Could not open %s\n", filename);
-        exit(1);
-    }
-
-    frame = av_frame_alloc();
-    if (!frame) {
-        fprintf(stderr, "Could not allocate video frame\n");
-        exit(1);
-    }
-    frame->format = c->pix_fmt;
-    frame->width = c->width;
-    frame->height = c->height;
-
-    ret = av_frame_get_buffer(frame, 0);
-    if (ret < 0) {
-        fprintf(stderr, "Could not allocate the video frame data\n");
-        exit(1);
-    }
-
-    /* encode 1 second of video */
-    for (i = 0; i < 3000; i++) {
-        fflush(stdout);
-
-        /* Make sure the frame data is writable.
-           On the first round, the frame is fresh from av_frame_get_buffer()
-           and therefore we know it is writable.
-           But on the next rounds, encode() will have called
-           avcodec_send_frame(), and the codec may have kept a reference to
-           the frame in its internal structures, that makes the frame
-           unwritable.
-           av_frame_make_writable() checks that and allocates a new buffer
-           for the frame only if necessary.
-         */
-        ret = av_frame_make_writable(frame);
-        if (ret < 0) {
+    // Prepare to start writing packets
+    avcodec_open2(cdctx, codec, NULL);
+    AVFrame* frame = av_frame_alloc();
+    frame->format = cdctx->pix_fmt;
+    frame->width = cdctx->width;
+    frame->height = cdctx->height;
+    av_frame_get_buffer(frame, 0);
+    AVPacket* pkt = av_packet_alloc();
+    int num_frames = 300;
+    for (int i = 0; i < num_frames; i++) {
+        av_frame_make_writable(frame);
+        prepare_frame_multithreaded(i, frame);
+        frame->pts = i;
+        int send_frame_result = avcodec_send_frame(cdctx, frame);
+        if (send_frame_result < 0) {
+            fprintf(stderr, "Error sending a frame for encoding\n");
             exit(1);
         }
-        prepare_frame_multithreaded(i, frame);
+        int receive_packet_result = 1;
+        while (receive_packet_result >= 0) {
+            receive_packet_result = avcodec_receive_packet(cdctx, pkt);
+            if (receive_packet_result == AVERROR(EAGAIN) || receive_packet_result == AVERROR_EOF)
+                break;
+            else if (receive_packet_result < 0) {
+                fprintf(stderr, "Error during encoding\n");
+                exit(1);
+            }
 
-        frame->pts = i;
-
-        /* encode the image */
-        printf("Encoding frame %d\n", i);
-        encode(c, frame, pkt, f);
+            printf("Write packet %3"PRId64" (size=%5d)\n", pkt->pts, pkt->size);
+            av_packet_rescale_ts(pkt, cdctx->time_base, stream->time_base);
+            av_write_frame(formatCtx, pkt);
+        }
     }
-
-    /* flush the encoder */
-    encode(c, NULL, pkt, f);
-
-    /* Add sequence end code to have a real MPEG file.
-       It makes only sense because this tiny examples writes packets
-       directly. This is called "elementary stream" and only works for some
-       codecs. To create a valid file, you usually need to write packets
-       into a proper file format or protocol; see muxing.c.
-     */
-    if (codec->id == AV_CODEC_ID_MPEG1VIDEO || codec->id == AV_CODEC_ID_MPEG2VIDEO)
-        fwrite(endcode, 1, sizeof(endcode), f);
-    fclose(f);
-
-    avcodec_free_context(&c);
+    avcodec_send_frame(cdctx, NULL);
+    int receive_packet_result = avcodec_receive_packet(cdctx, pkt);
+    while (receive_packet_result != AVERROR_EOF) {
+        printf("Write packet %3"PRId64" (size=%5d)\n", pkt->pts, pkt->size);
+        av_packet_rescale_ts(pkt, cdctx->time_base, stream->time_base);
+        av_write_frame(formatCtx, pkt);   
+        receive_packet_result = avcodec_receive_packet(cdctx, pkt);
+    }
+    printf("Done writing packets\n");
+    av_write_trailer(formatCtx);
+    printf("Done writing trailer\n");
+    double end = clock();
+    double duration = (end - start) / CLOCKS_PER_SEC;
+    printf("Finished in %f seconds\n", duration);
+    avformat_free_context(formatCtx);
+    avcodec_free_context(&cdctx);
     av_frame_free(&frame);
     av_packet_free(&pkt);
-
-    return 0;
+    avio_close(ioCtx);
 }
